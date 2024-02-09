bbd8dde979a099ded664cb7b43a8ff1df3f9471c
SWCL0 <stan.w.c@icloud.com>
2024-02-09 09:27:40 +0100

    update boodschappenlijstje.php
    aanmaak array
    opvraag aantal producten
    opvraag producten voor lijstje

--- boodschappenlijst.php
+++ boodschappenlijst.php
@@ -1,5 +1,15 @@
 <?php
 
+$productenv = array();
 
+echo "Hoeveel producten wil je toevoegen? ";
+$aantalProducten = readline();
+
+for ($i = 1; $i <= $aantalProducten; $i++) {
+    echo "Product $i van $aantalProducten: ";
+    $producten[] = readline();
+}
+
+var_dump($producten);
 
 ?>
